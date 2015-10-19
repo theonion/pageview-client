c589be4190245613edf29c093578d51f52068a3b
Cameron Lowe <cam.m.lowe@gmail.com>
2015-10-19 12:21:22 -0500

    test & scope magic mock in testing

--- tests/test_trending_client.py
+++ tests/test_trending_client.py
@@ -12,45 +12,50 @@ class Response(object):
 
 mock_content = json.dumps([{
     "content_id": 2,
-    "score": 100
+    "score": 55
 }, {
     "content_id": 0,
-    "score": 97
+    "score": 67
 }, {
     "content_id": 1,
-    "score": 96
+    "score": 78
 }])
 
 mock_byte_content = json.dumps([{
     "content_id": 3,
-    "score": 95
+    "score": 20
 }, {
     "content_id": 5,
-    "score": 94
+    "score": 90
 }, {
     "content_id": 4,
-    "score": 93
+    "score": 80
 }]).encode('utf-8')
-
-mock_response = Response()
-mock_response.ok = True
-mock_response.content = mock_content
-requests.get = MagicMock(return_value=mock_response)
 
 
 def test_trendingclient_get():
+    mock_response = Response()
+    mock_response.ok = True
+    mock_response.content = mock_content
+    requests.get = MagicMock(return_value=mock_response)
     client = TrendingClient("example.com", "hello.json")
+
     content_ids = client.get("example")
     assert len(content_ids) == 3
-    for cid in (2, 0, 1):
-        assert cid in content_ids
+    assert content_ids[0] == 1
+    assert content_ids[1] == 0
+    assert content_ids[2] == 2
 
 
 def test_trendingclient_get_byte_response():
+    mock_response = Response()
+    mock_response.ok = True
     mock_response.content = mock_byte_content
     requests.get = MagicMock(return_value=mock_response)
+
     client = TrendingClient("example.com", "hello.json")
     content_ids = client.get("example")
     assert len(content_ids) == 3
-    for cid in (3, 5, 4):
-        assert cid in content_ids
+    assert content_ids[0] == 5
+    assert content_ids[1] == 4
+    assert content_ids[2] == 3
